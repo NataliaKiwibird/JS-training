ce3468fee7bf030d255b80532bf5318c83c7331a
Natalia Bubiak <natalia.kiwibird@gmail.com>
2020-04-23 12:20:53 +0100

    statements done

--- statements.js
+++ statements.js
@@ -32,3 +32,75 @@ if (password.length > 12 && password.includes("&" || "*" || "!")) {
     } else {
         console.log("bardzo słabe hasło") 
     }
+
+    // ZADANIE Z "IF"
+
+    let X = 50;
+    let Y = 30;
+
+    if (X >= Y) {
+        console.log("X jest większe lub równe Y")
+    }
+
+    // ZADANIE IF & ELSE
+
+    let Color = "blue";
+    let NewColor = "green";
+
+    if (Color === NewColor) {
+        console.log("Kolory się zgadzają")
+    } else {
+        console.log("Kolory się nie zgadzają")
+    };
+
+    // ZADANIE IF, ELSE IF & ELSE
+
+    let c = 100;
+    let d = 50;
+
+    if (c > d) {
+        console.log("c jest większe od d")
+    } else if (c == d) {
+        console.log("c jest równe d")
+    } else {
+        console.log("c jest mniejsze od d")
+    }
+
+    // ZADANIE ZE SWITCH:
+
+    let promo = "20%";
+
+    switch (promo) {
+        case "10%":
+            console.log("Skorzystaj z promocji 10%");
+            break;
+        case "20%":
+            console.log("Skorzystaj z promocji 20%");
+            break;
+        case "30%":
+            console.log("Skorzystaj z promocji 30%");
+            break;
+        default:
+            console.log("Dziś nie ma zadnych promocji");
+    }
+
+    // ZADANIE Z OPERATOREM WARUNKOWYM
+
+    let z = 10;
+
+    console.log((z % 2 === 0) ?  "Wyrazenie prawdziwe" : "Wyrazenie fałszywe")
+
+    // inna mozliwosc:
+    let check = (z % 2 === 0) ?  "Wyrazenie prawdziwe" : "Wyrazenie fałszywe";
+
+    // ZADANIE TRUDNE, LECZ NIE NUDNE
+
+    let e = 50;
+
+    if (e >= 100) {
+        console.log("e jest większe".toUpperCase())
+    } else if (e < 100 && e > 30) {
+        console.log("e jest średniakiem".toUpperCase())
+    } else {
+        console.log("e jest małe".toUpperCase())
+    }
