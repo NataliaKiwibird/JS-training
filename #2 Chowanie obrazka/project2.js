861e01d391bdc7b05040858486d7b56d498f9df3
Natalia Bubiak <natalia.kiwibird@gmail.com>
2020-05-02 12:08:44 +0100

    Project 2

---
+++ #2 Chowanie obrazka/project2.js
@@ -0,0 +1,17 @@
+const item1 = document.querySelector('.item1');
+const button = document.querySelector('.arrow');
+const icon = document.querySelector('.fas')
+
+
+const click = () => {
+    item1.classList.toggle('hide');
+
+    if (item1.classList.contains('hide')) {
+        icon.style.transform = 'rotate(180deg)'
+    } else {
+        icon.style.transform = 'rotate(0deg)'
+    };
+
+};
+
+button.addEventListener('click', click);
