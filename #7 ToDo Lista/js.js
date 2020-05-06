c922e8119674d35e42c2a567a3ec277d64d3a1c4
Natalia Bubiak <natalia.kiwibird@gmail.com>
2020-05-06 17:13:57 +0100

    add button

--- #7 ToDo Lista/js.js
+++ #7 ToDo Lista/js.js
@@ -1,24 +1,67 @@
-let $toDoinput; // miejsce, gdzie uzytkownik wpsiuje treść zadania
-let alertInfo; // info o braku zadań
-let addBtn; // przycisk ADD - dodawanie nowych elementów listy
-let ulList; // nasza lista zadań, tagi <ul></ul>
+let $todoinput; // miejsce, gdzie uzytkownik wpsiuje treść zadania
+let $alertInfo; // info o braku zadań
+let $addBtn; // przycisk ADD - dodawanie nowych elementów listy
+let $ulList; // nasza lista zadań, tagi <ul></ul>
+let $newTask; // nowo dodane LI, nowe zadanie
 
 const main = () => {
-    prepareDomElements();
-    prepareDomEvents();
+    prepareDOMElements();
+    prepareDOMEvents();
 };
 
 // pobieranie elementów listy:
-const prepareDomElements = () => {
-    $toDoinput = document.querySelector('.todoInput');
+const prepareDOMElements = () => {
+    $todoinput = document.querySelector('.todoInput');
     $alertInfo = document.querySelector('.alertInfo'); 
     $addBtn = document.querySelector('.addBtn'); 
-    $ulList = document.querySelector('.ulList'); 
+    $ulList = document.querySelector('.todoList ul');
 };
 
 // nadawanie nasłuchiwania:
-const prepareDomEvents = () => {
-    
+const prepareDOMEvents = () => {
+    $addBtn.addEventListener('click', addNewTask)
+};
+
+function addNewTask () {
+    if ($todoinput.value !== '') {
+        $newTask = document.createElement('li');
+        $newTask.innerText = $todoinput.value;
+        $ulList.appendChild($newTask);
+        $todoinput.value = '';
+        $alertInfo.innerText = '';
+        createToolsArea();
+    } else {
+        $alertInfo.innerText = 'Wpisz treść zadania!'
+    };
+};
+
+let $toolsPanel;
+let $completeBtn;
+let $editBtn;
+let $deleteBtn;
+
+function createToolsArea () {
+    $toolsPanel = document.createElement('div');
+    $toolsPanel.classList.add('tools');
+    $newTask.appendChild($toolsPanel);
+
+    $completeBtn = document.createElement('button');
+    $completeBtn.classList.add('complete');
+    $toolsPanel.appendChild($completeBtn);
+    $completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+
+    $editBtn = document.createElement('button');
+    $editBtn.classList.add('edit');
+    $toolsPanel.appendChild($editBtn);
+    $editBtn.innerHTML = 'EDIT';
+
+    $deleteBtn = document.createElement('button');
+    $deleteBtn.classList.add('delete');
+    $toolsPanel.appendChild($deleteBtn);
+    $deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+
 };
 
 document.addEventListener('DOMContentLoaded', main);
