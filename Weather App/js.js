12e77e04e2e15849fb7449dcf0476275f58fe98b
Natalia Bubiak <natalia.kiwibird@gmail.com>
2020-06-01 12:36:48 +0100

    weather app files

---
+++ Weather App/js.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+const cityName = document.querySelector('.city-name');
+const warning = document.querySelector('.warning');
+const photo = document.querySelector('.photo');
+
+const weather = document.querySelector('.weather');
+const temperature = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
